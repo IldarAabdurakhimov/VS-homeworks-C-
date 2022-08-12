078a361496073965923078463702d04c76fc57ca
Ildar <109942162+IldarAabdurakhimov@users.noreply.github.com>
2022-08-12 11:10:15 +0700

    finished  task1

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее");
+Console.WriteLine("Введите число А:");
+int numberA=int.Parse (Console.ReadLine());
+ Console.WriteLine ("Введите число B:");
+int numberB=int.Parse (Console.ReadLine());
+if (numberA>numberB)
+{
+    Console.WriteLine("Максимальное число:" + numberA);
+}
+if (numberB>numberA)
+{
+    Console.WriteLine("Максимальное число:" + numberB);
+    Console.WriteLine("Минимальное число:" + numberA);
+}   
