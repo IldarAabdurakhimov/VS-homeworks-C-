5b091f9160c5ea2490eea0a1a125821d0aa32ed2
Ildar <109942162+IldarAabdurakhimov@users.noreply.github.com>
2022-08-26 13:05:35 +0700

    corected  task12

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿      //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+      Console.Write("Введите число :");
+      int InputNum = Convert.ToInt32(Console.ReadLine());
+      int Res=0;
+
+      while (InputNum !=0)
+      {
+        Res+=InputNum %10;
+        InputNum /=10;
+    
+      }
+      Console.WriteLine($"сумма цифр в числах = {Res}");
