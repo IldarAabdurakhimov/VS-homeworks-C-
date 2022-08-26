ce4335d657b457603904f4a033b10c9ced5e40d7
Ildar <109942162+IldarAabdurakhimov@users.noreply.github.com>
2022-08-26 12:37:43 +0700

    finished  task11

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿int InputNumberA,
+    InputNumberB;
+
+Console.Write("Введите число A: ");
+InputNumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+InputNumberB = Convert.ToInt32(Console.ReadLine());
+
+int result = InputNumberA;
+
+for (int i = 2; i <= InputNumberB; i++)
+{
+    result *= InputNumberA;
+}
+
+Console.WriteLine($"{InputNumberA}^{InputNumberB} = {result}");
