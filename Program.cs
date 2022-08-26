d138b095a040de16929e97aee4fa2d946127652a
Ildar <109942162+IldarAabdurakhimov@users.noreply.github.com>
2022-08-26 13:29:20 +0700

    finished  task13

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int M = 8;
+int min = 777, max = int.MaxValue;
+int[] array = new int[M];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(min, max);
+}
+for (int i = 0; i < array.Length; i++)
+{
+Console.WriteLine(array[i] + " ");
+}
