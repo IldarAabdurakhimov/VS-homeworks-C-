d46217dc3fe45ee84665c2f4288159582d1bab95
Ildar <109942162+IldarAabdurakhimov@users.noreply.github.com>
2022-09-01 11:56:44 +0700

    finished  task17,18,19

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+int FillArray(int[] array)
+{
+  int counter = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    array[i]=  int.Parse(Console.ReadLine()) ;
+    if (array[i]>0) counter++;
+  }
+  Console.WriteLine(string.Join(",", array));
+  return counter;
+}
+int m;
+Console.WriteLine("Input number and put enter: ");
+m=int.Parse(Console.ReadLine());
+int [] array = new int[m] ;
+Console.WriteLine(" ");
+Console.WriteLine("Summ numbers > 0 : " + FillArray(array));
+
+//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Input b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+Console.WriteLine($"Intersection at a point: ({x}; {y})");
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Доп. задча: Задайте двумерный массив со случайными числами от -10 до 10. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.)
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// Сумма элементов главной диагонали: 1+9+2 = 12
+
+
+int FillArray(int[,] array)
+{
+  int diag = 0;
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+array[i, j] = new Random().Next(-10, 11);
+Console.Write(array[i, j] + " ");
+if (i==j) diag=diag+array[i, j];
+}
+Console.WriteLine(" ");
+ }
+  return diag;
+}
+
+int [,] array = new int[4,4] ;
+Console.WriteLine (FillArray(array));
