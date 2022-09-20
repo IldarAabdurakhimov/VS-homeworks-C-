9f0f8b1d4fc38d7b5971ad919a1574d21fcc0515
Ildar <109942162+IldarAabdurakhimov@users.noreply.github.com>
2022-09-20 18:16:54 +0700

    finished FinalTas

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿void FinalArray (String[] FirstArr)
+{
+  String[] SecondArr = new String[FirstArr.Length];
+for (int i = 0; i < SecondArr.Length; i++)
+{
+    if (FirstArr[i].Length <=3)
+    {
+        SecondArr[i] = FirstArr[i];
+        Console.WriteLine( $"[{SecondArr[i]}]");
+    }
+}
+}
+string[] FirstArr = new String[4]
+{"5646", "ddd", "dff", "dfd", };
+Console.WriteLine();
+FinalArray(FirstArr);
