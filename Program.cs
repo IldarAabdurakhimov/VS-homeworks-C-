716160a16e64fca471c57f5d7a520d76b7eb7bf3
Ildar <109942162+IldarAabdurakhimov@users.noreply.github.com>
2022-09-20 18:23:17 +0700

    Copied from fork  FinalTask2 to  FinalTask1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,16 @@
-﻿Void FinalArray (String[] FirtArr)
+void FinalArray (String[] FirstArr)
 {
-  String[] SecondArr = new String[FirtArr.Length];
-
+  String[] SecondArr = new String[FirstArr.Length];
+for (int i = 0; i < SecondArr.Length; i++)
+{
+    if (FirstArr[i].Length <=3)
+    {
+        SecondArr[i] = FirstArr[i];
+        Console.WriteLine( $"[{SecondArr[i]}]");
+    }
 }
+}
+string[] FirstArr = new String[4]
+{"5646", "ddd", "dff", "dfd", };
+Console.WriteLine();
+FinalArray(FirstArr);
